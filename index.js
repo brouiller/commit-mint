f9fb34c5a9082599cbcd632e14034776452a9d59
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-12-23 11:48:33 -0500

    consolidated code into one file and rearranged functions

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const fs = require("fs");
+const fetch = require("cross-fetch");
 
+//runs the program
 const init = () => {
   doesBatchFileExist();
-//   infiniteMonkey();
-
 };
 
 //checks to see if the commands batch file exits, if it doesn't, it writes the file
 const doesBatchFileExist = () => {
-  const commands = `git add .\ngit commit\ngit push`;
-  fs.stat("commands.bat", function (err, stat) {
+  const commands = `@echo off\nnode index.js`;
+  fs.stat("runNode.bat", function (err, stat) {
     if (err !== null) {
-      fs.writeFile("commands.bat", commands, (err) => {
+      fs.writeFile("runNode.bat", commands, (err) => {
         if (err) console.log(err);
         else {
           console.log("File written successfully\n");
           console.log("The written has the following contents:");
-          console.log(fs.readFileSync("commands.bat", "utf8"));
+          console.log(fs.readFileSync("runNode.bat", "utf8"));
         }
       });
-        execShellCommand(
-          `SCHTASKS /CREATE /SC DAILY /TN "CommitMint task" /TR "runNode.bat" /ST 11:00`
-        );
+      execShellCommand(
+        `SCHTASKS /CREATE /SC DAILY /TN "CommitMint task" /TR "runNode.bat" /ST 11:00`
+      );
     } else {
-          execShellCommand("commands.bat");
+      commitMint();
     }
   });
 };
@@ -43,6 +43,31 @@ const execShellCommand = (cmd) => {
       resolve(stdout ? true : false);
     });
   });
+};
+
+//runs the loop
+const commitMint = () => {
+  for (let i = 0; i < 20; i++) {
+    execShellCommand("git add .");
+    execShellCommand("git commit");
+      execShellCommand("git push origin main");
+      infiniteMonkey();
+  }
+};
+
+//generates a text file with random words in it
+async function infiniteMonkey() {
+  let monkey = await fetch(
+    "https://random-word-api.herokuapp.com/word?number=73&swear=0"
+  )
+    .then((response) => response.json())
+    .then((monkey) => {
+        let monkeyData = monkey.toString();
+        fs.writeFile("monkey-attempt.txt", monkeyData, (err) => {
+          //we're going to write any errors/successes to a log file
+        if (err) console.log(err);
+      });
+    });
 };
 
 init();
