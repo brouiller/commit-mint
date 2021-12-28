8075f8bd04cd44fca5ce671956a71a8b61a46407
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-12-28 11:49:46 -0500

    commit index: 0Tue Dec 28 2021 11:49:45 GMT-0500 (Eastern Standard Time)

--- index.js
+++ index.js
@@ -49,19 +49,19 @@ const commitMint = () => {
       fs.writeFile("currentTime.txt", stringI, (error) =>
         console.log("git error: ", error)
       );
-    }, 500 * (i ? i * 5 : 1));
+    }, 500 + (i ? i * 2500 : 1));
     setTimeout(() => {
       console.log("git add 1500 index: ",i)
       execShellCommand(`git add .\n`);
-    }, 1500 * (i ? i * 5 : 1));
+    }, 1000 + (i ? i * 2500 : 1));
     setTimeout(() => {
       console.log("git commit 2500 index: ", i);
       execShellCommand(`git commit -m "${stringI}"\n`);
-    }, 2500 * (i ? i * 5 : 1));
+    }, 1500 + (i ? i * 2500 : 1));
     setTimeout(() => {
       console.log("git push 3500 index: ", i);
       execShellCommand(`git push --force origin bradley\n`);
-    }, 3500 * (i ? i * 5 : 1));
+    }, 2000 + (i ? i * 2500 : 1));
   }
 };
 
