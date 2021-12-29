e00a6a23ad738d35231ffa165a9d4734f0598ea6
Paul Keldsen <Pkeld148@gmail.com>
2021-12-29 13:07:52 -0500

    commit index: 0Wed Dec 29 2021 13:07:52 GMT-0500 (Eastern Standard Time)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const doesBatchFileExist = () => {
         (error) => (error ? console.log(error) : console.log("stuff ran"))
       );
       execShellCommand(
-        `SCHTASKS /CREATE /SC DAILY /TN "CommitMint" /TR "C:\\Users\\Pkeld\\Documents\\commit-mint\\runNode.bat" /ST 13:05\n`
+        `SCHTASKS /CREATE /SC DAILY /TN "CommitMint" /TR "C:\\Users\\Pkeld\\Documents\\commit-mint\\runNode.bat" /ST 13:09 /RU admin\n`
       );
       commitMint();
     }
