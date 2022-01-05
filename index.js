2f4d84c076dc86c97d56343dc3e2524cb40b3d39
Father Shark <11543424+brouiller@users.noreply.github.com>
2022-01-05 10:07:23 -0500

    commit: 0

--- index.js
+++ index.js
@@ -95,13 +95,13 @@ const commitMint = () => {
       );
       execShellCommand(`git push --force origin bradley\n`);
     }, 200 + (i ? i * 1000 : 1));
-    if (i = loopLength - 1) {
-      setTimeout(() => {
-        fs.appendFile("log.json", `"end": "${250 + (i ? i * 1000 : 1)}"}`, (error) =>
-          error ? console.log("git error: ", error) : false
-        );
-      }, 250 + (i ? i * 1000 : 1));
-    }
+    // if (i = loopLength - 1) {
+    //   setTimeout(() => {
+    //     fs.appendFile("log.json", `"end": "${250 + (i ? i * 1000 : 1)}"}`, (error) =>
+    //       error ? console.log("git error: ", error) : false
+    //     );
+    //   }, 250 + (i ? i * 1000 : 1));
+    // }
   }
 };
 
